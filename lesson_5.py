67cea371e17e31002716df2da8d6e78e8ea06f5c
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-06-10 19:58:35 +0300

    les5

--- lesson_5.py
+++ lesson_5.py
@@ -83,10 +83,43 @@
 
 
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+fig, ax = plt.subplots()
+
+ax.spines[["left", "bottom"]].set_position(("data", 0))
+# Сховати верхню та праву лінію
+ax.spines[["top", "right"]].set_visible(False)
 
 
+# Намалюємо стрілки (як чорні трикутники: ">k"/"^k") на кінцях осей
+# Також вимкнемо відсікання (clip_on=False) стрілок
+ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
+ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
+
+
+# Додамо проміжні лінії
+ax.grid(True, linestyle='-.')
+
+
+# Сформуємо ряд значень x. 100 елементів від -5 до 5
+x = np.linspace(-5, 5, 100, False)
+
+
+# Функціональну залежність
+# ax.plot(x, 1/(x**2 + 2*x + 3))
+# ax.plot(x, 1/(x**2 - 2*x + 3))
+# ax.plot(x, np.sin(x) + 2)
+# ax.plot(x, np.sin(x) + - 1)
+# ax.plot(x, - x ** 2)
+ax.plot(x, np.cos(x))
+ax.plot(x, np.tan(x))
+ax.plot(x, np.cot(x))
+
+# Запускаємо малювання графіка
+plt.show()
 
 
 
