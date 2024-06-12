dfeaa053cd74872c9ee6a17e8fab853d42857368
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-06-12 23:15:36 +0300

    6

---
+++ lesson_6.py
@@ -0,0 +1,13 @@
+# Функціональну залежність
+# ax.plot(x, 1/(x**2 + 2*x + 3))
+# ax.plot(x, 1/(x**2 - 2*x + 3))
+# ax.plot(x, np.sin(x) + 2)
+# ax.plot(x, np.sin(x) + - 1)
+# ax.plot(x, - x ** 2)
+ax.plot(x, np.cos(x))
+ax.plot(x, np.tan(x))
+ax.plot(x, np.cot(x))
+
+# Запускаємо малювання графіка
+plt.show()
+
